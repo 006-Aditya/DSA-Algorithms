c546968d4dfecbff3e96367a8342f669df74e573
Aditya kumar <aditya512001@gmail.com>
2025-09-19 21:54:36 +0530

    String Matching

---
+++ String Matching/Trie Algorithm/1.cpp
@@ -0,0 +1,49 @@
+// problem on Trie --
+
+// code -- 
+
+class Solution {
+public:
+    struct trieNode{
+        int count;
+        trieNode* children[26];
+    };
+    trieNode* getNode(){
+        trieNode* newNode=new trieNode();
+        newNode->count=0;
+        for(int i=0;i<26;i++){
+            newNode->children[i]=NULL;
+        }
+        return newNode;
+    }
+    trieNode* root=getNode();
+    void insert(string word){
+        trieNode* Crawl=root;
+        for(char ch:word){
+            int idx=ch-'a';
+            if(Crawl->children[idx]==NULL){
+                Crawl->children[idx]=getNode();
+            }
+            Crawl=Crawl->children[idx];
+            Crawl->count+=1;
+        }
+    }
+    vector<int> sumPrefixScores(vector<string>& words) {
+        vector<int> res(words.size());
+        for(int i=0;i<words.size();i++){
+            insert(words[i]);
+        }
+        for(int i=0;i<words.size();i++){
+            string word=words[i];
+            int cnt=0;
+            trieNode* Crawl=root;
+            for(char ch:word){
+                int idx=ch-'a';
+                Crawl=Crawl->children[idx];
+                cnt+=Crawl->count;
+            }
+            res[i]=cnt;
+        }
+        return res;
+    }
+};
